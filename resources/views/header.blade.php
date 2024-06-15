d73c04890257b2c9d370638d1e2b47878d38ac65
Robertarezza <roberta.rezza@gmail.com>
2024-06-15 16:48:20 +0200

    fix: style home page

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -1,7 +1,10 @@
 <header>
-  <nav class="navbar navbar-expand-lg bg-light">
+  <nav class="navbar navbar-expand-lg bg-light my_nav">
     <div class="container-fluid">
-      <a class="navbar-brand" href="#">Comics</a>
+    <a class="navbar-brand" href="#">
+      <img src="{{ asset('img/logo.jpg') }}" alt="Bootstrap" width="85" height="50" class="logo">
+    </a>
+      <!-- <a class="navbar-brand" href="#">Comics</a> -->
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
         aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,9 +1,10 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container text-center">
-    <h3>Vai all'archivio dei Fumetti</h3>
-    <a href="{{ route('comics.index') }}" class="btn btn-success">Clicca Qui</a>
+<div class="container text-center home mt-3">
+    
+   <!-- <img src="/img/Comic_Book_Dash.webp" loading="lazy" alt=""> -->
+    <a href="{{ route('comics.index') }}" class="btn w-35" style="margin-top: 36rem; font-size: 3rem; color: transparent;">Clicca Qui</a>
 
 </div>
     
