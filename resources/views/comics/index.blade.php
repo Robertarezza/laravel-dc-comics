0d0aaf060f3dab587ef8555d53ce6ab3874a76cb
Robertarezza <roberta.rezza@gmail.com>
2024-06-15 12:02:50 +0200

    feat: table comics

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -2,10 +2,43 @@
 
 @section('content')
 <div class="container">
-    <h1>
-        Lista fumetti
-    </h1>
+    <h1>Lista fumetti</h1>
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">Id</th>
+                <th scope="col">Title</th>
+                <th scope="col">Series</th>
+                <th scope="col" class="w-25">Sale Date</th>
+                <th scope="col" class="w-25">Type</th>
+                <th scope="col">Price</th>
+                <th scope="col">Actions</th>
+                <!-- <th scope="col">Description</th> -->
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ( $comicList as $comic )
+            <tr>
+                <th scope="row">{{$comic->id}}</th>
+               
+                    <td class="d-flex">
+                        <img src="{{$comic->image}}" alt="" class="w-25 me-2">
+
+                        {{$comic->title}}
+                    </td>
+               
+                <td>{{$comic->series}}</td>
+                <td>{{$comic->sale_date}}</td>
+                <td>{{$comic->type}}</td>
+                <td>{{$comic->price}}</td>
+                <td><button type="submit" class="btn btn-success"><a href="">Dettagli</a></button></td>
+                <!-- <td>{{$comic->description}}</td> -->
+            </tr>
+            @endforeach
+
+        </tbody>
+    </table>
 
 </div>
-    
+
 @endsection
